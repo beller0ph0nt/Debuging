6b20b2abc557ba252ee51ccd181657e921e505fe
beller0ph0nt <kras_den@rambler.ru>
2015-04-12 23:57:49 +0700

    Добавлена многопоточная очередь

--- Debugging/Debugging/Log.cs
+++ Debugging/Debugging/Log.cs
@@ -7,37 +7,45 @@ namespace Debugging
 {
     public class Log
     {
-        private static object _locker = new object();
-        //private static AutoResetEvent _goingAheadThread;    // Впередиидущий поток
-        //private static AutoResetEvent _currentThread;       // Текущий поток
+        //private static object _locker = new object();
+        private static AutoResetEvent _goingAheadThread;    // Впередиидущий поток
+        private static AutoResetEvent _currentThread;       // Текущий поток
 
         private static void WriteAsync(object obj)
         {
-            //if (tmpPar._goingAheadThread != null)
-            //    tmpPar._goingAheadThread.WaitOne();
-
-            //if (tmpPar._currentThread != null)
-            //    tmpPar._currentThread.Set();
-        }
-
-        public static void Write(string message)
-        {
             try
             {
+                var currentDateTime = new DateTime(DateTime.Now.Ticks);
+                var tmpPar = obj as ThreadParameters;
+
+                if (tmpPar.goingAheadThread != null)
+                    tmpPar.goingAheadThread.WaitOne();
+
                 string logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log");
 
                 if (!Directory.Exists(logPath))
                     Directory.CreateDirectory(logPath);
 
                 string filename = Path.Combine(logPath, string.Format("{0}_{1:dd.MM.yyy}.log", AppDomain.CurrentDomain.FriendlyName, DateTime.Now));
-                string fullText = string.Format("[{0:dd.MM.yyy HH:mm:ss.fff}] {1}\r\n", DateTime.Now, message);
+                string fullText = string.Format("[{0:dd.MM.yyy HH:mm:ss.fff}] {1}\r\n", currentDateTime, tmpPar.message);
 
-                lock (_locker)
-                {
-                    File.AppendAllText(filename, fullText, Encoding.GetEncoding("Windows-1251"));
-                }
+                //lock (_locker)
+                //{
+                File.AppendAllText(filename, fullText, Encoding.GetEncoding("Windows-1251"));
+                //}
+
+                if (tmpPar.currentThread != null)
+                    tmpPar.currentThread.Set();
             }
             catch (Exception ex) { throw ex; }
+        }
+
+        public static void Write(string message)
+        {
+            _goingAheadThread = _currentThread;
+            _currentThread = new AutoResetEvent(false);
+            ThreadPool.QueueUserWorkItem(WriteAsync, 
+                new ThreadParameters(new string(message.ToCharArray()), _goingAheadThread, _currentThread));
         }
     }
 }
---
+++ Debugging/Debugging/ThreadParameters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Debugging
+{
+    public class ThreadParameters
+    {
+        public string message { get; private set; }
+        public AutoResetEvent goingAheadThread { get; private set; }
+        public AutoResetEvent currentThread { get; private set; }
+
+        public ThreadParameters(string message, AutoResetEvent goingAheadThread, AutoResetEvent currentThread)
+        {
+            this.message = message;
+            this.goingAheadThread = goingAheadThread;
+            this.currentThread = currentThread;
+        }
+    }
+}
